18c0148ddbafbffbbc51caa8fb15bc72d7c4ff62
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-12-19 15:07:08 +0100

    First implementation of the logic

--- SSReader/SSReader.ConsoleApp/Program.cs
+++ SSReader/SSReader.ConsoleApp/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Tesseract;
 
 namespace SSReader.ConsoleApp
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProcessImage();
+        }
+
+        private static void ProcessImage()
+        {
+            Image rawImageFromClipboard = Clipboard.GetImage();
+            Bitmap bitmap = new Bitmap(rawImageFromClipboard);
+            Pix imageCompatibleWithTesseract = PixConverter.ToPix(bitmap);
+            using var engine = new TesseractEngine(@"./tessdata", "eng+jpn");
+            using var page = engine.Process(imageCompatibleWithTesseract);
+            var text = page.GetText();
+            Clipboard.SetText(text);
+
         }
     }
 }
